57ba6419fa3aee5188509260789708ec7d3050e8
PabloGarciaa7 <pablomh.97@gmail.com>
2023-04-21 13:34:34 +0200

    Creando el repositorio y primero archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { ProductoComponent } from './producto/producto.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    UsuarioComponent,
+    ProductoComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
